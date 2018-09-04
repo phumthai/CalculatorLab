b508492c144b1138111583d85451e835f6584fc1
phumthai <31661931+phumthai@users.noreply.github.com>
2018-09-04 19:56:06 +0700

    RPN engine test : pass PS.  The first programe file is crash. It can't push to github. I don't know how to fix it. That why I clone and do it again.

--- CPE200Lab1/CPE200Lab1/ExtendForm.cs
+++ CPE200Lab1/CPE200Lab1/ExtendForm.cs
@@ -16,11 +16,13 @@ namespace CPE200Lab1
         private bool isContainDot = false;
         private bool isSpaceAllowed = false;
         private CalculatorEngine engine;
+        private RPNCalculatorEngine RPNengine;
 
         public ExtendForm()
         {
             InitializeComponent();
             engine = new CalculatorEngine();
+            RPNengine = new RPNCalculatorEngine();
         }
 
         private bool isOperator(char ch)
@@ -104,7 +106,15 @@ namespace CPE200Lab1
             string result = engine.Process(lblDisplay.Text);
             if (result is "E")
             {
-                lblDisplay.Text = "Error";
+                result = RPNengine.Process(lblDisplay.Text);
+                if (result == "E")
+                {
+                    lblDisplay.Text = "Error";
+                }
+                else
+                {
+                    lblDisplay.Text = result;
+                }
             } else
             {
                 lblDisplay.Text = result;
--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -6,11 +6,45 @@ using System.Threading.Tasks;
 
 namespace CPE200Lab1
 {
-    public class RPNCalculatorEngine
+    public class RPNCalculatorEngine : CalculatorEngine
     {
         public string Process(string str)
         {
+            Stack<string> numbers = new Stack<string>();
+            string[] parts = str.Split(' ');
+            if (parts.Length == 1)
+            {
+                return "E";
+            }
+            for (int i = 0; i < parts.Length; i++)
+            {
+                if (isNumber(parts[i]))
+                {
+                    numbers.Push(parts[i]);
+                }
+                else if (isOperator(parts[i]))
+                {
+                    if (numbers.Count < 2)
+                    {
+                        return "E";
+                    }
+                    string st, nd;
+                    nd = numbers.Peek();
+                    numbers.Pop();
+                    st = numbers.Peek();
+                    numbers.Pop();
+                    numbers.Push(calculate(parts[i], st, nd, 8));
+                }
 
+            }
+            if (numbers.Count == 1)
+            {
+                return numbers.Peek();
+            }
+            else
+            {
+                return "E";
+            }
             // your code here
             return "E";
         }
